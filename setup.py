8dbd43c2c1a1124e5ff4c311e2639e99d0e6697e
Omar Ryhan <omarryhan@gmail.com>
2018-09-04 14:10:59 +0200

    Fixed pypi repo name issues

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask-stateless-auth",
-    version="0.0.1",
+    version="0.0.3",
     author="Omar Ryhan",
     author_email="omarryhan@gmail.com",
     description="Flask stateless authentication with unsigned tokens",
