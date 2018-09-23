4764fc6343883844e73812d33d552f8a1967e64e
Omar Ryhan <omarryhan@gmail.com>
2018-09-23 17:38:24 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask-stateless-auth",
-    version="0.0.",
+    version="0.0.15",
     author="Omar Ryhan",
     author_email="omarryhan@gmail.com",
     license="MIT",
